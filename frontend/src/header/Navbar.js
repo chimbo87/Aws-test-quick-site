0f0c32286b1a1a5b1e66787df3d2011015eaf94b
chimbo87 <archfordnhanga@gmail.com>
2024-01-17 15:46:22 +0200

    commit all

---
+++ frontend/src/header/Navbar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg" id="theHeader">
+      <div class="container-fluid"  id="navBar">
+        <a class="navbar-brand" href="#">
+          GeeDataVisualisation
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#" id="navLinks">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#" id="navLinks">
+                About Us
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#" id="navLinks">
+                Services
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#" id="navLinks">
+                Contact Us
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#" id="navLinks">
+                Privacy
+              </a>
+            </li>
+          </ul>
+          <div id="navBtn">
+            <button>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/pages/home/Home.js
@@ -0,0 +1,194 @@
+import React from "react";
+import "./Home.css";
+import aboutImg from "../../assets/about.jpeg";
+function Home() {
+  return (
+    <>
+      <div className="container-fluid" id="homePage">
+        <div id="homebox">
+          <h1>
+            Transform raw data into captivating narratives, Your journey to
+            meaningful visualization starts here!
+          </h1>
+          <button>Get Started</button>
+        </div>
+      </div>
+      <div className="container">
+        <div class="row" id="offerBox">
+          <h4>What We Do</h4>
+          <div class="col-lg-4 col-md-4">
+            <div id="offerCard">
+              <i class="bx bx-objects-vertical-bottom"></i>
+              <h6>Lorem Ipsum is simply dummy text</h6>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged
+              </p>
+              <button>Read More</button>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4">
+            <div id="offerCard">
+              <i class="bx bxl-deezer"></i>
+              <h6>Lorem Ipsum is simply dummy text</h6>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged
+              </p>
+              <button>Read More</button>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4">
+            <div id="offerCard">
+              <i class="bx bx-circle-three-quarter"></i>
+              <h6>Lorem Ipsum is simply dummy text</h6>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged
+              </p>
+              <button>Read More</button>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4">
+            <div id="offerCard">
+              <i class="bx bx-scatter-chart"></i>
+              <h6>Lorem Ipsum is simply dummy text</h6>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged
+              </p>
+              <button>Read More</button>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4">
+            <div id="offerCard">
+              <i class="bx bxs-component"></i>
+              <h6>Lorem Ipsum is simply dummy text</h6>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged
+              </p>
+              <button>Read More</button>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4">
+            <div id="offerCard">
+              <i class="bx bx-pie-chart-alt-2"></i>
+              <h6>Lorem Ipsum is simply dummy text</h6>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged
+              </p>
+              <button>Read More</button>
+            </div>
+          </div>
+        </div>
+        <div class="row" id="missionBox">
+          <div class="col-lg-6 col-md-4">
+            <div id="missionCard">
+              <img src={aboutImg}></img>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-4">
+            <div id="missionCard">
+              <h2>
+                {" "}
+                Explore the power of visual storytelling and uncover insights
+                that drive informed decisions.
+              </h2>
+              <button>Become A Client</button>
+            </div>
+          </div>
+        </div>
+        <div id="contactBox">
+          <h4>Get In Touch With Us</h4>
+          <div class="row">
+            <div class="col-lg-8 col-md-4">
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">
+                  Full Name
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="exampleFormControlInput1"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleFormControlInput1"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">
+                  Message
+                </label>
+                <textarea
+                  class="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                ></textarea>
+              </div>
+              <div id="contactBtn">
+                <button>Submit</button>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4">
+              <div id="contactCardWrap">
+                <div id="contactCardWrapBox">
+                  <i class="bx bx-map"></i>
+                  <p>
+                    123 Mars Street. <br></br>Jupiter Mars
+                  </p>
+                </div>
+                <div id="contactCardWrapBox">
+                  <i class="bx bx-envelope"></i>
+                  <p>
+                    text@example.com <br></br>text@example.com
+                  </p>
+                </div>
+                <div id="contactCardWrapBox">
+                  <i class="bx bx-phone"></i>
+                  <p>
+                    +123 234 2345 <br></br>+123 234 2345
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
